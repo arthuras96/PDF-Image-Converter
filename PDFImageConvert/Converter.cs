93b4655b3fbed1933f3f5e6128f9158a3681484f
arthuras96 <arthuralencarsilva@gmail.com>
2021-10-27 16:58:45 -0300

    initial commit

---
+++ PDFImageConvert/Converter.cs
@@ -0,0 +1,94 @@
+﻿using PdfiumViewer;
+using PdfSharp.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFImageConvert
+{
+    public class Converter
+    {
+        public bool ConvertPDFToImage(string pathPDF, string pathToSaveIMG, int dpi, long quality)
+        {
+			if (File.Exists(pathPDF))
+			{
+				try
+				{
+					using (var document = PdfDocument.Load(pathPDF))
+					{
+						var pageCount = document.PageCount;
+
+						for (int i = 0; i < pageCount; i++)
+						{
+							using (var image = document.Render(i, dpi, dpi, PdfRenderFlags.CorrectFromDpi))
+							{
+								var encoder = ImageCodecInfo.GetImageEncoders().First(c => c.FormatID == ImageFormat.Jpeg.Guid);
+								var encParams = new EncoderParameters(1);
+								encParams.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+								image.Save(pathToSaveIMG + Path.GetFileNameWithoutExtension(pathPDF) + " - Pag. " + (i + 1).ToString("D6") + ".jpg", encoder, encParams);
+							}
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+					return false;
+				}
+				return true;
+			}
+			else
+			{
+				Console.WriteLine("Falha ao buscar o arquivo " + pathPDF + ".");
+				return false;
+			}
+		}
+
+		public bool ConvertImageToPDF(List<string> imageFileNameWithPath, string pdfFileNameWithPath, int width)
+		{
+			if(imageFileNameWithPath.Count > 0)
+            {
+                try 
+				{ 
+					Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+					using (var document = new PdfSharp.Pdf.PdfDocument())
+					{
+						foreach (string imageFileName in imageFileNameWithPath)
+						{
+							PdfSharp.Pdf.PdfPage page = document.AddPage();
+							using (XImage img = XImage.FromFile(imageFileName))
+							{
+								// Calculate new height to keep image ratio
+								var height = (int)(((double)width / (double)img.PixelWidth) * img.PixelHeight);
+
+								// Change PDF Page size to match image
+								page.Width = width;
+								page.Height = height;
+
+								XGraphics gfx = XGraphics.FromPdfPage(page);
+								gfx.DrawImage(img, 0, 0, width, height);
+							}
+						}
+						document.Save(pdfFileNameWithPath);
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+					return false;
+				}
+
+				return true;
+			}
+			else
+            {
+				Console.WriteLine("Nenhuma imagem foi localizada na pasta.");
+				return false;
+			}
+		}
+	}
+}
---
+++ PDFImageConvert/Program.cs
@@ -0,0 +1,32 @@
+﻿using PdfiumViewer;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace PDFImageConvert
+{
+    class Program
+    {
+		public static string projectDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+		static void Main(string[] args)
+        {
+			string pathToSave = projectDir + @"\Samples\";
+			string pathPDF = projectDir + @"\Samples\ENEM 2009.pdf";
+
+			Converter converter = new Converter();
+			converter.ConvertPDFToImage(pathPDF, pathToSave, 200, 25);
+
+			List<string> imagesToConvert = new List<string>();
+			string[] files = Directory.GetFiles(pathToSave);
+			foreach (string file in files)
+				if (file.Split('.')[file.Split('.').Length - 1].ToLower() == "jpg")
+					imagesToConvert.Add(file);
+			imagesToConvert.Sort();
+
+			converter.ConvertImageToPDF(imagesToConvert, pathToSave + "ENEM 2009 reconvertido.pdf", 1000);
+		}
+	}
+}
